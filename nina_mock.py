a3a94234cf64965790f4760382284b0a2ace4d78
Cesare Diodato <diodatocesare2001@gmail.com>
2024-11-06 16:33:25 +0100

    added no type messages

--- nina_mock.py
+++ nina_mock.py
@@ -22,29 +22,34 @@ client = mqtt.Client(client_id="NINA_Mock", callback_api_version=mqtt.CallbackAP
 client.connect(broker_address, 1883, 60)
 
 # Funzione per inviare messaggi di N.I.N.A.
-def send_nina_message(msg_type: MessageType, message: str):
-    print(f"Invio del messaggio di tipo {msg_type.value}...")
+def send_nina_message(message: str, msg_type: MessageType = None):
+    print(f"Invio del messaggio...")
     payload = {
-        "type": msg_type.value,
         "message": message
     }
+    if msg_type is not None:
+        payload["type"] = msg_type.value
     client.publish(topic, json.dumps(payload))
     print(f"Messaggio inviato: {payload}")
 
 # Simulazione di invio messaggi
 while True:
-    # Simula il messaggio di stato di calibrazione
-    send_nina_message(MessageType.CALIBRATION_STATUS, "Calibration successful.")
-    time.sleep(5)  # Simula un intervallo tra i messaggi
-
-    # Simula l'inizio di un'osservazione
-    send_nina_message(MessageType.OBSERVATION_STARTED, "Observation started.")
+    # Messaggio con tipo di stato di calibrazione
+    send_nina_message("Calibration successful.", MessageType.CALIBRATION_STATUS)
     time.sleep(5)
 
-    # Simula la fine di un'osservazione
-    send_nina_message(MessageType.OBSERVATION_ENDED, "Observation ended.")
+    # Messaggio di inizio osservazione
+    send_nina_message("Observation started.", MessageType.OBSERVATION_STARTED)
     time.sleep(5)
 
-    # Simula un errore
-    send_nina_message(MessageType.ERROR, "An error occurred in the observation process.")
+    # Messaggio di fine osservazione
+    send_nina_message("Observation ended.", MessageType.OBSERVATION_ENDED)
+    time.sleep(5)
+
+    # Messaggio di errore
+    send_nina_message("An error occurred in the observation process.", MessageType.ERROR)
+    time.sleep(5)
+
+    # Messaggio senza tipo
+    send_nina_message("This is a message without a specific type.")
     time.sleep(5)
